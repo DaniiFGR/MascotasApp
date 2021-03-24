11ac46d6d765c36b7ea43fd7db3e4d492c2ab35d
Daniela Garrido <daniifgr09@gmail.com>
2021-03-23 22:02:48 -0500

    Conexion bdmascotas exito

--- src/componentes/ListaMascotas.js
+++ src/componentes/ListaMascotas.js
@@ -1,15 +1,37 @@
 //IMPORTAR COMPONENTES
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import axios from 'axios';
 
 //CUERPO
 class ListaMascotas extends Component {
+    state = { ListaMascotas:[] };
+    componentDidMount(){
+       axios.get("https://givecars.herokuapp.com/")
+         //axios.get("http://192.168.100.49:8080/WSRMascotas/webresources/udenar.mascotas/")
+        .then(response => {
+            this.setState({ListaMascotas: response.data });
+            console.log(response);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+      
+
+    }
     render(){
-         return ( <View ><Text>sadasdas</Text></View> );
+         return ( <View ><Text>Mascotitasssss!</Text></View> );
     }
     //return <View style={styles.estiloVista}>{props.children}</View>
    
 }
+
+
+
+
+
+
+
 
 //ESTILOS
 const styles={
